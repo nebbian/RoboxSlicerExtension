9d025ea9dbeba3d6fa058a336b7d889ccfa62ed1
Ben Hitchcock <ben@zensoft.com.au>
2017-06-16 23:41:57 +0800

    + New code to parse the GCode in a more generic manner

--- shell scripts/slic3r_postprocess.pl
+++ shell scripts/slic3r_postprocess.pl
@@ -5,6 +5,7 @@
 
 use strict;
 use warnings;
+use Math::Round;
 
 my $layerCount = 0;
 my @lines;
@@ -25,7 +26,7 @@ while (<INPUT>) {
 
 seek INPUT, 0, 0;
 
-my $MIN_EXTRUSION_LENGTH = 0.3;		# Minimum extrusion length before we will allow a retraction
+my $MIN_EXTRUSION_LENGTH = 0.0;		# Minimum extrusion length before we will allow a retraction
 my $MIN_TRAVEL_DISTANCE = 0.01;  # Minimum distance of travel before we actually take the command seriously
 
 my $oldHint = '';
@@ -42,6 +43,7 @@ my $travelMoveLastFileSize = 0;
 my $lastMoveWasTravel = 0;
 my $lastMoveWasRetract = 0;
 my $extrusionAfterRetraction = 0;
+my $totalExtrusion = 0;
 my $retracted = 0;
 
 
@@ -75,32 +77,83 @@ while (<INPUT>) {
 		# Remove use absolute coordinates
 	} elsif (/^(M83\s)/) {
 		# Remove use relative distances for extrusion
-	} elsif (/^(DISABLED_G1\s+)/){ 
+	} elsif (/^(G1\s+)/){ 
 		EXIT_IF:{
+			
+			my $outputCommand = "";		
 		
 			# Grab all possible commands
 			if(/(X([\-0-9\.]+)\s)/) {$commandX = $2} else {$commandX = 'false'}
 			if(/(Y([\-0-9\.]+)\s)/) {$commandY = $2} else {$commandY = 'false'}
-			if(/(Z([\-0-9\.]+)\s)/) {$commandZ = $2} else {$commandY = 'false'}
+			if(/(Z([\-0-9\.]+)\s)/) {$commandZ = $2} else {$commandZ = 'false'}
 			if(/(E([\-0-9\.]+)\s)/) {$commandE = $2} else {$commandE = 'false'}
-			if(/(F([\-0-9\.]+)\s)/) {$commandSpeed = $2} else {$commandSpeed = 'false'}
-			if(/;\s+(.+)$/) {$comment = $2} else {$comment = 'false'}
+			if(/(F([\-0-9\.]+)\s)/) {$commandSpeed = round($2)} else {$commandSpeed = 'false'}
+			if(/;\s+(.+)$/) {$comment = $1} else {$comment = 'false'}
 		
 			# Output hints
 			if($comment eq "brim") { $hint = "SKIRT";}
 			if($comment eq "perimeter") { $hint = "WALL-OUTER"; }
 			if($comment eq "infill") { $hint = "SKIN"; }
+			if($comment eq "support material") { $hint = "SUPPORT"; }
+			if($comment eq "support material interface") { $hint = "SUPPORT"; }
 
 			if($hint ne $oldHint){
 				print NEW ";TYPE:$hint\n";
 				$oldHint = $hint;
-				last;	# Break out of the if statement
+				# last;	# Break out of the if statement
+			}
+			
+			# Figure out if this is a travel move or not
+			if($commandE ne 'false'){
+				$outputCommand = 'G1';	# Printing move
+			}
+			else {
+				$outputCommand = 'G0';	# Travel move
 			}
 		
-			if($commandSpeed != 'false'){
+			### Build command for output
+			
+			# Output speed if present
+			if($commandSpeed ne 'false'){
+				$currentSpeed = $commandSpeed;
 			}
-		
 
+			# Remember the Z position if required
+			if($commandZ ne 'false'){
+				
+				$currentZ = $commandZ;
+				$oldHint = "";
+			}
+			
+			# Output the X and Y position if required
+			if(($commandX ne 'false') && ($commandY ne 'false')){
+				$outputCommand .= sprintf " F%s X%s Y%s Z%s", $currentSpeed, $commandX, $commandY, $currentZ;
+			}
+			
+			# 
+			if($commandE ne 'false'){
+				#Find retract/unretract
+				if(($commandX eq 'false') && ($commandY eq 'false')){
+					# Retract/unretract
+					
+					# Print extrusion if not straight after the first layer change
+					if($totalExtrusion > 0){
+						$outputCommand .= sprintf " E%s", $commandE;
+					}
+				}
+				else {
+					# Normal print move
+					$outputCommand .= sprintf " E%s", $commandE;
+				}
+				$totalExtrusion += $commandE;
+				
+			}
+			
+			
+			## Send the command to the file
+			if(length($outputCommand) > 2){
+				printf NEW "%s\n", $outputCommand;
+			}
 		}	
 	} elsif (/^(G1\s+Z([0-9\.]+)\s+)/){
 		# Layer change code
