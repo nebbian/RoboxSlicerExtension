c1dc6de467182a2d25dd92c18d7fc89c09784460
Ben Hitchcock <ben@SSEnterprise.gateway>
2017-07-31 17:04:39 +0800

    Updates to Java flow, which enables the Flow application to work on Windows.  Changes include handling line separators properly, and using a windows-compatible path for the link to Slic3r.

--- java/robox-slicer-extension/robox-slicer-flow/src/main/java/com/roboxing/slicerextension/flow/Slicer.java
+++ java/robox-slicer-extension/robox-slicer-flow/src/main/java/com/roboxing/slicerextension/flow/Slicer.java
@@ -68,6 +68,8 @@ public abstract class Slicer {
     }
 
     public void postProcess(File inputGCode, File resultGCode) throws IOException{
+        String newLine = System.lineSeparator();
+
         // isAbsoluteExtrusion = false;
         java.util.regex.Pattern patternAbsolute = java.util.regex.Pattern.compile("^(M82\\s)");
         java.util.regex.Pattern patternExtrusion = java.util.regex.Pattern.compile("(E([\\-0-9\\.]+)\\s?)");
@@ -122,13 +124,13 @@ public abstract class Slicer {
                         // String newLine = strLine.replace(m.group(0), String.format(Locale.ROOT, "E%.5g%n", newExtrusion));
                         String newLine = strLine.replace(m.group(0), "E"+(double)Math.round(newExtrusion * 100000d) / 100000d);
                         // System.out.println("new Line :" + newLine);
-                        outputRelative.write(newLine+"\n");
+                        outputRelative.write(newLine + newLine);
                     } else {
-                        outputRelative.write(strLine+"\n");
+                        outputRelative.write(strLine + newLine);
                     }
                     previousExtrusion = currentExtrusion;
                 } else {
-                    outputRelative.write(strLine+"\n");
+                    outputRelative.write(strLine + newLine);
                 }
             }
             // if extrusion was absolute use the relative E converted file as input
@@ -180,7 +182,7 @@ public abstract class Slicer {
                 }
                 if ((m = java.util.regex.Pattern.compile("^(M204\\s+S(\\d+))").matcher(strLine)).find()) {
                     // printf NEW "M201 X%d Y%d Z%d E2000\n", $2, $2, $2;
-                    writeOutput(String.format("M201 X%d Y%d Z%d E2000\n", m.group(2), m.group(2), m.group(2)));
+                    writeOutput(String.format("M201 X%d Y%d Z%d E2000" + newLine, m.group(2), m.group(2), m.group(2)));
                 } else if (java.util.regex.Pattern.compile("^(M190\\s)").matcher(strLine).find()) {
                     // Remove bed temperature settings
                 } else if (java.util.regex.Pattern.compile("^(M104\\s)").matcher(strLine).find()) {
@@ -236,7 +238,7 @@ public abstract class Slicer {
                     }
 
                     if (hint != oldHint) {
-                        writeOutput(";TYPE:" + hint + "\n");
+                        writeOutput(";TYPE:" + hint + newLine);
                         oldHint = hint;
                     }
 
@@ -303,21 +305,21 @@ public abstract class Slicer {
                     if ((printMoveValid == true) && (outputCommand.length() > 2)){
                         // printf NEW "%s\n", $outputCommand;
                         // LOGGER.info("output : "+outputCommand);
-                        writeOutput(String.format("%s\n",outputCommand));
+                        writeOutput(String.format("%s" + newLine,outputCommand));
                     }
                 }  else if (java.util.regex.Pattern.compile("^(;LAYER:0)").matcher(strLine).find()) {
                     // Output the layer count
-                    writeOutput(String.format(";Layer count: %d\n", layerCount));
-                    writeOutput(strLine + "\n");
+                    writeOutput(String.format(";Layer count: %d" + newLine, layerCount));
+                    writeOutput(strLine + newLine);
                     // extrusionAfterRetraction = 0;
                 } else if (java.util.regex.Pattern.compile("^(;LAYER:)").matcher(strLine).find()) {
                     // Output the layer number
-                    writeOutput(strLine + "\n");
+                    writeOutput(strLine + newLine);
                     // $extrusionAfterRetraction = 0;
                 }  else {
                     // System.out.println(strLine);
                     // StandardCharsets.UTF_8.encode(strLine).array()
-                    writeOutput(strLine + "\n");
+                    writeOutput(strLine + newLine);
                 }
 
                 // Save the current position
